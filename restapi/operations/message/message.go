99da5185228c498dba3b48b448ed5e743e7025a7
megascizor <tueda1207@gmail.com>
2018-09-04 07:15:40 +0900

    <message> added (no validation, no error handler)

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -1,14 +1,56 @@
 package message
 
 import (
+	"github.com/eure/si2018-server-side/entities"
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
+	// r := repositories.NewUserMessageRepository()
+	//
+	// err := r.Create()
+	//
+	// if err != nil {
+	// 	return si.NewPostMessageInternalServerError().WithPayload(
+	// 		&si.PostMessageInternalServerErrorBody{
+	// 			Code:    "500",
+	// 			Message: "Internal Server Error",
+	// 		})
+	// }
+	// if ent == nil {
+	// 	return si.NewPostMessageBadRequest().WithPayload(
+	// 		&si.PostMessageBadRequestBody{
+	// 			Code: "400",
+	// 			Message: "Bad Request",
+	// 		})
+	// }
+
 	return si.NewPostMessageOK()
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
-	return si.NewGetMessagesOK()
+	r := repositories.NewUserMessageRepository()
+	mr := repositories.NewUserMatchRepository()
+	ur := repositories.NewUserRepository()
+
+	uEnt, _ := ur.GetByToken(p.Token)
+
+	// Add error handling
+
+	parentIDs, _ := mr.FindAllByUserID(uEnt.ID)
+
+	// Add error handling
+
+	var ents entities.UserMessages
+	for _, parentID := range parentIDs {
+		messages, _ := r.GetMessages(uEnt.ID, parentID, int(*p.Limit), p.Latest, p.Oldest)
+		for _, message := range messages {
+			ents = append(ents, message)
+		}
+	}
+
+	sEnt := ents.Build()
+	return si.NewGetMessagesOK().WithPayload(sEnt)
 }
