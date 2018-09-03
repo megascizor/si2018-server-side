6ae2c7525bb1826f530deb4753ee8abf38eeb3d9
megascizor <tueda1207@gmail.com>
2018-09-04 07:15:04 +0900

    <match> added (no validation, no error handler)

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -1,10 +1,31 @@
 package usermatch
 
 import (
+	"github.com/eure/si2018-server-side/entities"
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
 )
 
 func GetMatches(p si.GetMatchesParams) middleware.Responder {
-	return si.NewGetMatchesOK()
+	ur := repositories.NewUserRepository()
+	mr := repositories.NewUserMatchRepository()
+
+	uEnt, _ := ur.GetByToken(p.Token)
+
+	mlEnts, _ := mr.FindByUserIDWithLimitOffset(uEnt.ID, int(p.Limit), int(p.Offset))
+	var ents entities.MatchUserResponses
+	for _, matchUser := range mlEnts {
+		var res = entities.MatchUserResponse{}
+		// user, err := ur.GetByUserID(matchUser.UserID)
+
+		// Add error handler
+
+		user, _ := ur.GetByUserID(matchUser.UserID)
+		res.ApplyUser(*user)
+		ents = append(ents, res)
+	}
+
+	sEnt := ents.Build()
+	return si.NewGetMatchesOK().WithPayload(sEnt)
 }
