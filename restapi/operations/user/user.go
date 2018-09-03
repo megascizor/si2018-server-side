acaec6995703db9a564212d7f27c1bd0cb388452
megascizor <tueda1207@gmail.com>
2018-09-03 23:46:33 +0900

    <user:profile> created (no validator)

--- restapi/operations/user/user.go
+++ restapi/operations/user/user.go
@@ -3,15 +3,56 @@ package user
 import (
 	"github.com/go-openapi/runtime/middleware"
 
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 )
 
 func GetUsers(p si.GetUsersParams) middleware.Responder {
+	// ur := repositories.NewUserRepository()
+	// tr := repositories.NewUserTokenRepository()
+	//
+	// token_user, err := tr.GetByToken(p.Token)
+	//
+	// user, err := ur.GetByUserID(token.UserID)
+	//
+	// p.Limit = 20
+	// p.Offset = 0
+	//
+	// var ids []int64
+	// users, err := ur.FindWithCondition(p.Limit, p.Offset, user.Gender, ids)
+	//
 	return si.NewGetUsersOK()
 }
 
 func GetProfileByUserID(p si.GetProfileByUserIDParams) middleware.Responder {
-	return si.NewGetProfileByUserIDOK()
+	r := repositories.NewUserRepository()
+
+	ent, err := r.GetByUserID(p.UserID)
+
+	if err != nil {
+		return si.NewGetProfileByUserIDInternalServerError().WithPayload(
+			&si.GetProfileByUserIDInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if ent == nil {
+		return si.NewGetProfileByUserIDNotFound().WithPayload(
+			&si.GetProfileByUserIDNotFoundBody{
+				Code:    "404",
+				Message: "User Profile Not Found",
+			})
+	}
+	// if .Token != token.Token {
+	// 	return si.NewGetProfileByUserIDUnauthorized().WithPayload(
+	// 		&si.GetProfileByUserIDUnauthorizedBody{
+	// 			Code:    "401",
+	// 			Message: "Unauthorized",
+	// 		})
+	// }
+
+	sEnt := ent.Build()
+	return si.NewGetProfileByUserIDOK().WithPayload(&sEnt)
 }
 
 func PutProfile(p si.PutProfileParams) middleware.Responder {
