de1a92b656c50fb9477a51c233695efe4494b8b0
megascizor <tueda1207@gmail.com>
2018-09-07 04:19:32 +0900

    <message> modified (Todo: list message in descending order)

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -4,23 +4,99 @@ import (
 	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
+	"github.com/eure/si2018-server-side/restapi/utils"
 	"github.com/go-openapi/runtime/middleware"
+	strfmt "github.com/go-openapi/strfmt"
+	"time"
 )
 
+// PostMessage post message
 func PostMessage(p si.PostMessageParams) middleware.Responder {
-	r := repositories.NewUserMessageRepository()
-	ur := repositories.NewUserRepository()
+	repoUser := repositories.NewUserRepository()
+	repoUserMatch := repositories.NewUserMatchRepository()
+	repoUserMessage := repositories.NewUserMessageRepository()
+	repoUserToken := repositories.NewUserTokenRepository()
 
-	sendUser, _ := ur.GetByToken(p.Params.Token)
-	/*
-	* Add error handling
-	 */
+	// Validation
+	entUserToken, err := repoUserToken.GetByToken(p.Params.Token)
+	if err != nil {
+		return si.NewPostMessageInternalServerError().WithPayload(
+			&si.PostMessageInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if entUserToken == nil {
+		return si.NewPostMessageUnauthorized().WithPayload(
+			&si.PostMessageUnauthorizedBody{
+				Code:    "401",
+				Message: "Unauthorized",
+			})
+	}
 
-	var ent = entities.UserMessage{}
-	ent.UserID = sendUser.ID
-	ent.PartnerID = p.UserID
+	// Get users (sender, receiver)
+	sendUser, err := repoUser.GetByUserID(entUserToken.UserID)
+	if err != nil {
+		return si.NewPostMessageInternalServerError().WithPayload(
+			&si.PostMessageInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if sendUser == nil {
+		return si.NewPostMessageBadRequest().WithPayload(
+			&si.PostMessageBadRequestBody{
+				Code:    "404",
+				Message: "Bad Request: 'GetByUserID' (sender) failed: " + err.Error(),
+			})
+	}
 
-	err := r.Create(ent)
+	recvUser, err := repoUser.GetByUserID(p.UserID)
+	if err != nil {
+		return si.NewPostMessageInternalServerError().WithPayload(
+			&si.PostMessageInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if recvUser == nil {
+		return si.NewPostMessageBadRequest().WithPayload(
+			&si.PostMessageBadRequestBody{
+				Code:    "404",
+				Message: "Bad Request: 'GetByUserID' (receiver) failed" + err.Error(),
+			})
+	}
+
+	sendID := sendUser.ID
+	recvID := recvUser.ID
+
+	// Check whether to match
+	matchedIDs, errMatch := repoUserMatch.FindAllByUserID(sendID)
+	if errMatch != nil {
+		return si.NewPostMessageInternalServerError().WithPayload(
+			&si.PostMessageInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+
+	if !utils.IsContained(recvID, matchedIDs) {
+		return si.NewPostMessageBadRequest().WithPayload(
+			&si.PostMessageBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request: not mathced",
+			})
+	}
+
+	// Apply parameters (message)
+	var entMessage entities.UserMessage
+	entMessage.UserID = sendID
+	entMessage.PartnerID = recvID
+	entMessage.Message = p.Params.Message
+	entMessage.CreatedAt = strfmt.DateTime(time.Now())
+	entMessage.UpdatedAt = strfmt.DateTime(time.Now())
+
+	err = repoUserMessage.Create(entMessage)
 	if err != nil {
 		return si.NewPostMessageInternalServerError().WithPayload(
 			&si.PostMessageInternalServerErrorBody{
@@ -29,32 +105,114 @@ func PostMessage(p si.PostMessageParams) middleware.Responder {
 			})
 	}
 
-	return si.NewPostMessageOK()
+	return si.NewPostMessageOK().WithPayload(
+		&si.PostMessageOKBody{
+			Code:    "200",
+			Message: "OK",
+		})
 }
 
+// GetMessages get messages
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
-	r := repositories.NewUserMessageRepository()
-	mr := repositories.NewUserMatchRepository()
-	ur := repositories.NewUserRepository()
+	repoUser := repositories.NewUserRepository()
+	repoUserMatch := repositories.NewUserMatchRepository()
+	repoUserMessage := repositories.NewUserMessageRepository()
+	repoUserToken := repositories.NewUserTokenRepository()
 
-	uEnt, _ := ur.GetByToken(p.Token)
-	/*
-	* Add error handling
-	 */
+	// Validation
+	if *p.Limit <= 0 {
+		return si.NewGetMessagesBadRequest().WithPayload(
+			&si.GetMessagesBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request: limit in query must be more than 1",
+			})
+	}
+	/* Todo: Add validater for p.Latest and p.Oldest */
 
-	parentIDs, _ := mr.FindAllByUserID(uEnt.ID)
-	/*
-	* Add error handling
-	 */
-
-	var ents entities.UserMessages
-	for _, parentID := range parentIDs {
-		messages, _ := r.GetMessages(uEnt.ID, parentID, int(*p.Limit), p.Latest, p.Oldest)
-		for _, message := range messages {
-			ents = append(ents, message)
-		}
+	entUserToken, err := repoUserToken.GetByToken(p.Token)
+	if err != nil {
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if entUserToken == nil {
+		return si.NewGetMessagesUnauthorized().WithPayload(
+			&si.GetMessagesUnauthorizedBody{
+				Code:    "401",
+				Message: "Unauthorized",
+			})
 	}
 
-	sEnt := ents.Build()
+	// Get users' entities (sender, receiver)
+	sendUser, err := repoUser.GetByUserID(entUserToken.UserID)
+	if err != nil {
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if sendUser == nil {
+		return si.NewGetMessagesBadRequest().WithPayload(
+			&si.GetMessagesBadRequestBody{
+				Code:    "404",
+				Message: "Bad Request: 'GetByUserID' (sender) failed: " + err.Error(),
+			})
+	}
+
+	recvUser, err := repoUser.GetByUserID(p.UserID)
+	if err != nil {
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if recvUser == nil {
+		return si.NewGetUsersBadRequest().WithPayload(
+			&si.GetUsersBadRequestBody{
+				Code:    "404",
+				Message: "Bad Request: 'GetByUserID' (receiver) failed: " + err.Error(),
+			})
+	}
+
+	sendID := sendUser.ID
+	recvID := recvUser.ID
+
+	// Check whether to match
+	matchedIDs, err := repoUserMatch.FindAllByUserID(sendID)
+	if err != nil {
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+
+	if !utils.IsContained(recvID, matchedIDs) {
+		return si.NewGetMessagesBadRequest().WithPayload(
+			&si.GetMessagesBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request: not matched",
+			})
+	}
+
+	// Get messages
+	var entMessages entities.UserMessages
+	entMessages, err = repoUserMessage.GetMessages(sendID, recvID, int(*p.Limit), p.Latest, p.Oldest)
+	if err != nil {
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	// for _, message := range messages {
+	// 	entMessages = append(entMessages, message)
+	// }
+
+	sEnt := entMessages.Build()
 	return si.NewGetMessagesOK().WithPayload(sEnt)
 }
